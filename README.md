662474c2cbb44443887c9b076186a0fc98124291
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-07-09 13:09:10 -0700

    First pass using `vscode.commands.executeCommand`

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# Good and Bad Formatter
+# Reindenter #
 
+A simple VSCode formatter extension that wraps `editor.action.reindentlines`.
 
-This is a sample for a simple (and stupid) formatter that integrates into VS Code in two way: Once as a [comm](https://github.com/jrieken/vscode-formatter-sample/blob/master/src/extension.ts#L8)[and](https://github.com/jrieken/vscode-formatter-sample/blob/master/package.json#L31) and once (better) 
-via the [formatter](https://github.com/jrieken/vscode-formatter-sample/blob/master/src/extension.ts#L23)-api. The latter as the following advantages:
+This allows things like formatting on save and "Format Document".
 
-* is less code
-* is invoked from the formatter actions and keybindings
-* is invoked from format on save
-* is future proof for things like *format all files in folders* etc
+Based on example code by [jrieken]( https://github.com/jrieken/vscode-formatter-sample).
+
+This extension is language agnostic.  So long as your language extension has indentation rules this should work for you.
+
+Tested against VS Code 1.35.1
--- src/extension.ts
+++ src/extension.ts
@@ -3,29 +3,11 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-
-    // 👎 formatter implemented as separate command
-    vscode.commands.registerCommand('extension.format-foo', () => {
-        const {activeTextEditor} = vscode.window;
-
-        if (activeTextEditor && activeTextEditor.document.languageId === 'foo-lang') {
-            const {document} = activeTextEditor;
-            const firstLine = document.lineAt(0);
-            if (firstLine.text !== '42') {
-                const edit = new vscode.WorkspaceEdit();
-                edit.insert(document.uri, firstLine.range.start, '42\n');
-                return vscode.workspace.applyEdit(edit)
-            }
-        }
-    });
-
-    // 👍 formatter implemented using API
-    vscode.languages.registerDocumentFormattingEditProvider('foo-lang', {
+    vscode.languages.registerDocumentFormattingEditProvider('ruby', {
         provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-            const firstLine = document.lineAt(0);
-            if (firstLine.text !== '42') {
-                return [vscode.TextEdit.insert(firstLine.range.start, '42\n')];
-            }
+            vscode.commands.executeCommand('editor.action.reindentlines');
+
+            return [];
         }
     });
 }
